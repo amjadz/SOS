29d735b39797df1c0943fb47eed66a66b65298a0
Zubair Amjad <amjadz@uw.edu>
2018-11-29 15:28:12 -0800

    Litteraly this stupid

--- app/src/main/java/edu/uw/acevedoj/sos/OtherFragment.kt
+++ app/src/main/java/edu/uw/acevedoj/sos/OtherFragment.kt
@@ -10,7 +10,7 @@ import android.view.ViewGroup
 class OtherFragment: Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_sms, null)
+        return inflater.inflate(R.layout.fragment_other, null)
     }
 
 
--- app/src/main/java/edu/uw/acevedoj/sos/SMSFragment.kt
+++ app/src/main/java/edu/uw/acevedoj/sos/SMSFragment.kt
@@ -9,6 +9,7 @@ import android.content.IntentFilter
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
+import android.support.v4.app.AppComponentFactory
 import android.support.v4.app.Fragment
 import android.support.v4.content.ContextCompat
 import android.telephony.SmsManager
@@ -32,80 +33,82 @@ class SMSFragment: Fragment() {
 
     }
 
-    private fun sendSOSSMS(){
-        val sentPI = PendingIntent.getBroadcast(this, 0, Intent(SENT), 0)
-        val delivered = PendingIntent.getBroadcast(this,0, Intent(DELIVERED), 0)
+    // This is the autoSMS code will be used as soon as I figure out fragments
 
-        text_contact.setOnClickListener {
-            val sms = SmsManager.getDefault()
-
-            sms.sendTextMessage("5554",null, "I need help", sentPI, delivered)
-
-        }
-
-    }
-
-    private fun checkForSmsPermission(){
-        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.SEND_SMS)) {
-
-            } else {
-                ActivityCompat.requestPermissions(
-                    this,
-                    arrayOf(android.Manifest.permission.SEND_SMS),
-                    MY_PERMISSIONS_REQUEST_SEND_SMS
-                )
-            }
-        }
-    }
-
-    override fun onResume() {
-        super.onResume()
-
-        smsSent = object: BroadcastReceiver() {
-            override fun onReceive(context: Context, intent: Intent) {
-
-                when(resultCode){
-
-                    Activity.RESULT_OK ->
-                        Toast.makeText(context, "SMS sent!", Toast.LENGTH_SHORT).show()
-
-                    SmsManager.RESULT_ERROR_GENERIC_FAILURE ->
-                        Toast.makeText(context, "Failure", Toast.LENGTH_SHORT).show()
-
-                    SmsManager.RESULT_ERROR_NO_SERVICE ->
-                        Toast.makeText(context, "No Service!", Toast.LENGTH_SHORT).show()
-
-                }
-            }
-        }
-
-        smsDelivered = object: BroadcastReceiver(){
-            override fun onReceive(context: Context, intent: Intent) {
-                when(resultCode){
-
-                    Activity.RESULT_OK ->
-                        Toast.makeText(context, "SMS Delivered!", Toast.LENGTH_SHORT).show()
-
-                    SmsManager.RESULT_ERROR_GENERIC_FAILURE ->
-                        Toast.makeText(context, "SMS Not Delivered!", Toast.LENGTH_SHORT).show()
-
-                }
-
-            }
-
-        }
-
-        registerReceiver(smsSent, IntentFilter(SENT))
-        registerReceiver(smsDelivered, IntentFilter(DELIVERED))
-
-    }
-
-    override  fun onPause() {
-        super.onPause()
-
-        unregisterReceiver(smsSent)
-        unregisterReceiver(smsDelivered)
-    }
+//    private fun sendSOSSMS(){
+//        val sentPI = PendingIntent.getBroadcast(this, 0, Intent(SENT), 0)
+//        val delivered = PendingIntent.getBroadcast(this,0, Intent(DELIVERED), 0)
+//
+//        text_contact.setOnClickListener {
+//            val sms = SmsManager.getDefault()
+//
+//            sms.sendTextMessage("5554",null, "I need help", sentPI, delivered)
+//
+//        }
+//
+//    }
+//
+//    private fun checkForSmsPermission(){
+//        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+//            if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.SEND_SMS)) {
+//
+//            } else {
+//                ActivityCompat.requestPermissions(
+//                    this,
+//                    arrayOf(android.Manifest.permission.SEND_SMS),
+//                    MY_PERMISSIONS_REQUEST_SEND_SMS
+//                )
+//            }
+//        }
+//    }
+//
+//    override fun onResume() {
+//        super.onResume()
+//
+//        smsSent = object: BroadcastReceiver() {
+//            override fun onReceive(context: Context, intent: Intent) {
+//
+//                when(resultCode){
+//
+//                    Activity.RESULT_OK ->
+//                        Toast.makeText(context, "SMS sent!", Toast.LENGTH_SHORT).show()
+//
+//                    SmsManager.RESULT_ERROR_GENERIC_FAILURE ->
+//                        Toast.makeText(context, "Failure", Toast.LENGTH_SHORT).show()
+//
+//                    SmsManager.RESULT_ERROR_NO_SERVICE ->
+//                        Toast.makeText(context, "No Service!", Toast.LENGTH_SHORT).show()
+//
+//                }
+//            }
+//        }
+//
+//        smsDelivered = object: BroadcastReceiver(){
+//            override fun onReceive(context: Context, intent: Intent) {
+//                when(resultCode){
+//
+//                    Activity.RESULT_OK ->
+//                        Toast.makeText(context, "SMS Delivered!", Toast.LENGTH_SHORT).show()
+//
+//                    SmsManager.RESULT_ERROR_GENERIC_FAILURE ->
+//                        Toast.makeText(context, "SMS Not Delivered!", Toast.LENGTH_SHORT).show()
+//
+//                }
+//
+//            }
+//
+//        }
+//
+//        registerReceiver(smsSent, IntentFilter(SENT))
+//        registerReceiver(smsDelivered, IntentFilter(DELIVERED))
+//
+//    }
+//
+//    override  fun onPause() {
+//        super.onPause()
+//
+//        unregisterReceiver(smsSent)
+//        unregisterReceiver(smsDelivered)
+//    }
 
 }
