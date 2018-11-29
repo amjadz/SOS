58240b285b75b9934634c4949c18742c9fc24b71
Zubair Amjad <amjadz@uw.edu>
2018-11-29 13:37:01 -0800

    Restored code

--- app/src/main/java/edu/uw/acevedoj/sos/MainActivity.kt
+++ app/src/main/java/edu/uw/acevedoj/sos/MainActivity.kt
@@ -1,7 +1,6 @@
 package edu.uw.acevedoj.sos
 
 import android.app.Activity
-import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -9,81 +8,66 @@ import android.content.IntentFilter
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
-
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.telephony.SmsManager
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.fragment_home.*
+import java.util.jar.Manifest
 
+private val MY_PERMISSIONS_REQUEST_READ_SMS = 3
+val SENT: String = "SMS_Sent"
+val DELIVERED: String = "SMS_DELIVERED"
+lateinit var smsSent: BroadcastReceiver
+lateinit var smsDelivered: BroadcastReceiver
 
-class  MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+class  MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                text_contact.setOnClickListener {
+                    val message = "I am in trouble, please help me"
 
+                    val sms = SmsManager.getDefault()
 
+                    sms.sendTextMessage("5554", null, message, null, null)
 
-        val navigation: BottomNavigationView = findViewById(R.id.navigation)
-        navigation.setOnNavigationItemSelectedListener(this)
-    }
+                }
 
-    private fun loadFragment(fragment: Fragment?): Boolean {
-        if (fragment != null) {
-            supportFragmentManager
-                .beginTransaction()
-                .replace(R.id.fragment_container,fragment)
-                .commit()
-            return true
-
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_dashboard -> {
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_notifications -> {
+                return@OnNavigationItemSelectedListener true
+            }
         }
-        return false
-
+        false
     }
-
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        checkForSmsPermission()
-        sendSOSSMS()
+
+        checkForSmsReadPermission()
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
 
-    private fun sendSOSSMS(){
-        val sentPI = PendingIntent.getBroadcast(this, 0, Intent(SENT), 0)
-        val delivered = PendingIntent.getBroadcast(this,0, Intent(DELIVERED), 0)
+    // My (Zubair) code from YAMA Assignment
+    private fun checkForSmsReadPermission(){
+        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED){
 
-        text_contact.setOnClickListener {
-            checkForSmsPermission()
 
-            val sms = SmsManager.getDefault()
-
-            sms.sendTextMessage("5554",null, "I need help", sentPI, delivered)
-
+        } else {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(android.Manifest.permission.READ_SMS),
+                MY_PERMISSIONS_REQUEST_READ_SMS)
         }
 
     }
-
-    private fun checkForSmsPermission(){
-        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.SEND_SMS)) {
-
-            } else {
-                ActivityCompat.requestPermissions(
-                    this,
-                    arrayOf(android.Manifest.permission.SEND_SMS),
-                    MY_PERMISSIONS_REQUEST_SEND_SMS
-                )
-            }
-        }
-    }
-
     override fun onResume() {
         super.onResume()
 
