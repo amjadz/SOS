7894d2f887217a4f9a3ecdd1f2faf5777445b195
Zubair Amjad <amjadz@uw.edu>
2018-11-29 15:01:52 -0800

    KILL ME

--- app/src/main/java/edu/uw/acevedoj/sos/OtherFragment.kt
+++ app/src/main/java/edu/uw/acevedoj/sos/OtherFragment.kt
@@ -10,7 +10,7 @@ import android.view.ViewGroup
 class OtherFragment: Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_him, null)
+        return inflater.inflate(R.layout.fragment_sms, null)
     }
 
 
