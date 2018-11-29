a3e8ba595b50042a4959777f92ef4a586b0c05f7
Zubair Amjad <amjadz@uw.edu>
2018-11-29 13:18:24 -0800

    Working on it

--- app/src/main/java/edu/uw/acevedoj/sos/MainActivity.kt
+++ app/src/main/java/edu/uw/acevedoj/sos/MainActivity.kt
@@ -2,9 +2,21 @@ package edu.uw.acevedoj.sos
 
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+<<<<<<< Updated upstream
 import android.support.v7.app.AppCompatActivity
+=======
+import android.support.v4.app.ActivityCompat
+import android.support.v4.app.Fragment
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
+import android.telephony.SmsManager
+import android.view.MenuItem
+import android.widget.Toast
+>>>>>>> Stashed changes
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_home.*
 
+<<<<<<< Updated upstream
 class MainActivity : AppCompatActivity() {
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
@@ -24,11 +36,64 @@ class MainActivity : AppCompatActivity() {
         }
         false
     }
+=======
+class  MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+
+>>>>>>> Stashed changes
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+<<<<<<< Updated upstream
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
+=======
+
+
+        val navigation: BottomNavigationView = findViewById(R.id.navigation)
+        navigation.setOnNavigationItemSelectedListener(this)
+    }
+
+    private fun loadFragment(fragment: Fragment?): Boolean {
+        if (fragment != null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment_container,fragment)
+                .commit()
+            return true
+
+        }
+        return false
+
+    }
+
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+        var fragment:Fragment? = null
+
+        when(p0.itemId){
+            R.id.navigation_home -> {
+                fragment = SMSFragment()
+
+            }
+
+            R.id.navigation_notifications -> {
+                fragment = DashboardFragment()
+
+            }
+
+            R.id.navigation_dashboard -> {
+                fragment = OtherFragment()
+
+            }
+
+        }
+
+
+        return loadFragment(fragment)
+    }
+
+
+
+>>>>>>> Stashed changes
 }
